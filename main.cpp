27810c9b7ecff29d631d7df3883c02fc43dfe8c4
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-09-30 11:33:37 +0900

    01_01 確認課題

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
-//#include <iostream>
 #include <stdio.h>
 
 // PG proj
 // LE2A_06_オザワ
 
-int main() {
+template<class T>
+T Min(T _dig0, T _dig1);
 
-	printf("Hello World\n");
-	printf("ハローワールド\n");
+template<>
+inline char Min<char>(char _char0, char _char1);
+
+int main(void) {
+
+	printf("int      : %d\n", Min<int>(8, 1));
+	printf("float    : %f\n", Min<float>(3.14f, 12.13f));
+	printf("double   : %f\n", Min<double>(93.14, 12.13));
+	printf("char     : %c\n", Min<char>('あ', 'い'));
 
 	return 0;
+}
+
+template<class T>
+T Min(T _dig0, T _dig1)
+{
+	T result = _dig0;
+	if (result > _dig1)
+		result = _dig1;
+
+	return static_cast<T>(result);
+};
+
+template<>
+inline char Min<char>(char _char0, char _char1)
+{
+	printf("数字以外は代入できません\n");
+	return '-';
 }
