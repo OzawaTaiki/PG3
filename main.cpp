f1ca93b83e10171b8c36bca21ca5a6fa19b00289
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-09-30 12:24:55 +0900

    01_02 確認課題

--- main.cpp
+++ main.cpp
@@ -4,13 +4,29 @@
 // PG proj
 // LE2A_06_オザワ
 
+// 給料を計算
 int CalculateSalary(int _salary, int _time, bool _isRecursize);
+
+// 超える時間を計算
+int FindHoursUntilRecursiveSalaryExceeds(int _normal, int _recursive);
 
 int main() {
 
-	int result= CalculateSalary(100, 10, true);
+	const int kWages = 1072;
+	const int kstartWages = 100;
+	int time = 1;
+	bool isRecursive = false;
 
-	std::cout << result << std::endl;
+	std::cout << "給料体系 0:一般 1:再帰" << std::endl;	
+	std::cin >> isRecursive;
+
+	std::cout << "働いた時間" << std::endl;	
+	std::cin >> time;
+
+	int result = CalculateSalary(isRecursive ? kstartWages : kWages, time, isRecursive);
+	std::cout << "給料 : " << result << std::endl;
+
+	//std::cout << FindHoursUntilRecursiveSalaryExceeds(1072,100) << std::endl;
 
 	return 0;
 }
@@ -25,8 +41,23 @@ int CalculateSalary(int _salary, int _time, bool _isRecursize)
 
 	else
 	{
-		return CalculateSalary(_salary, _time - 1, _isRecursize);
+		return CalculateSalary(_salary * 2 - 50 , _time - 1, _isRecursize);
 	}
 
-	return 0;
+}
+
+int FindHoursUntilRecursiveSalaryExceeds(int _normal, int _recursive)
+{
+	int normal = _normal;
+	int recursive = _recursive;
+	int time = 0;
+
+	while (normal > recursive)
+	{
+		time++;
+		normal = CalculateSalary(_normal, time, false);
+		recursive = CalculateSalary(_recursive, time, true);
+	}
+
+	return time;
 }
