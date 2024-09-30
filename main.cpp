f8829f0b3777036bfdaad54db45cd613b9184b40
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-09-30 11:38:19 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
-//#include <iostream>
+#include <iostream>
 #include <stdio.h>
 
 // PG proj
 // LE2A_06_オザワ
 
+int CalculateSalary(int _salary, int _time, bool _isRecursize);
+
 int main() {
 
-	printf("Hello World\n");
-	printf("ハローワールド\n");
+	int result= CalculateSalary(100, 10, true);
+
+	std::cout << result << std::endl;
+
+	return 0;
+}
+
+int CalculateSalary(int _salary, int _time, bool _isRecursize)
+{
+	if (_time <= 1)
+		return _salary;
+
+	if (!_isRecursize)
+		return _salary * _time;
+
+	else
+	{
+		return CalculateSalary(_salary, _time - 1, _isRecursize);
+	}
 
 	return 0;
 }
