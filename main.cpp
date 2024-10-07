b5281c6ebe4ded88774fc650c6df9eb72da96ed5
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-10-07 12:36:19 +0900

    01_04

--- main.cpp
+++ main.cpp
@@ -6,29 +6,33 @@
 // PG proj
 // LE2A_06_オザワ
 
-typedef void (*pFunc)(int);
-
-int Input();
-int GetAnswer();
-void SetTimeOut(int _second);
-std::string CheckAns(bool _flag);
+void SetTimeout(int _sec);
 
 int main() {
 
-	pFunc p = SetTimeOut;
-
-	int in =Input();
-	
-	std::function<bool()> f = [in](int _ans) { return _ans == in; };
-
-	CheckAns();
-
-	return 0;
-}
-
-int Input()
-{
 	int in = -1;
+
+	std::function<int()> getAnswer;
+	std::function<void()> checkAns;
+
+	getAnswer = []() {
+		std::mt19937 random;
+		std::random_device rd;
+		random.seed(rd());
+		std::uniform_real_distribution<>dist(1, 6);
+		int ans = static_cast<int> (dist(random));
+		std::cout << "出目:" << ans << std::endl;
+		return ans % 2;
+		};
+
+	checkAns = [&]() {
+		if (in == getAnswer() % 2)
+			std::cout << "正解" << std::endl;
+		else
+			std::cout << "不正解" << std::endl;
+		};
+
+
 	std::cout << "偶数なら０，基数なら１を入力" << std::endl;
 	while (1)
 	{
@@ -38,28 +42,14 @@ int Input()
 		else
 			std::cout << "０か１以外受け付けない" << std::endl;
 	}
-	return in;
+
+	SetTimeout(3);
+	checkAns();
+
+	return 0;
 }
 
-int GetAnswer()
+void SetTimeout(int _sec)
 {
-	std::mt19937 random;
-	std::random_device rd;
-	random.seed(rd());
-	std::uniform_real_distribution<>dist(0, 99);
-	int ans = static_cast<int> (dist(random));
-	return ans % 2;
-}
-
-void SetTimeOut(int _second)
-{
-	Sleep(_second * 1000);
-}
-
-std::string  CheckAns(bool _flag)
-{
-	if (_flag)
-		return "正解";
-	else
-		return "不正解";
+	Sleep(_sec * 1000);
 }
