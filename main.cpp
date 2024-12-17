fa13d371ec7bc563fd3952510475f97fa64d44f4
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2024-12-17 22:58:01 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
-//#include <iostream>
-#include <stdio.h>
+#include <iostream>
+//#include <stdio.h>
+
+#include <thread>
 
 // PG proj
 // LE2A_06_オザワ
 
 int main() {
 
-	printf("Hello World\n");
-	printf("ハローワールド\n");
+
+    std::thread thread1([]() {std::cout << "thread1" << std::endl; });
+    thread1.join();
+
+    std::thread thread2([]() {std::cout << "thread2" << std::endl; });
+    thread2.join();
+
+    std::thread thread3([]() {std::cout << "thread3" << std::endl; });
+    thread3.join();
 
 	return 0;
 }
