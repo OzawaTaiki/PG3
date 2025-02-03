1b90a62108ed73877848327fee1d08a6d9ff370a
OzawaTaiki <146906402+OzawaTaiki@users.noreply.github.com>
2025-02-03 12:42:12 +0900

    sampleとかなり時間差があるけど

--- main.cpp
+++ main.cpp
@@ -1,13 +1,39 @@
-//#include <iostream>
-#include <stdio.h>
+#include <iostream>
+
+#include <string>
+#include <vector>
+#include <chrono>
 
 // PG proj
 // LE2A_06_オザワ
 
-int main() {
+int main()
+{
+    std::chrono::steady_clock::time_point start, end;
 
-	printf("Hello World\n");
-	printf("ハローワールド\n");
+    std::string a(100000, 'a');
 
-	return 0;
+    start = std::chrono::high_resolution_clock::now();
+
+    std::string copy = a;
+
+    end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Copy time: "
+        << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+        << " μs" << std::endl;
+
+
+    start = std::chrono::high_resolution_clock::now();
+
+    std::string move = std::move(a);  // ムーブ
+
+    end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Move time: "
+        << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+        << " μs" << std::endl;
+
+
+    return 0;
 }
